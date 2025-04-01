97627aa19e8d028c75415694e7714465ec803ea8
shravan kumar <117439152+borrolla@users.noreply.github.com>
2025-04-01 20:43:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function removeSelectedColor(){
+	const colorSelect = document.getElementById("colorSelect");
+	colorSelect.remove(colorSelect.selectedIndex);
+}
+// Attach the function to the button
+document.querySelector('input[type="button"]').addEventListener("click", 
+													removeSelectedColor);
